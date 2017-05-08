4be559b0e24abb5f6790a95e920c3bdec860aa75
Michael Monsour <michael@lethek.net>
2017-05-08 14:18:44 +1000

    Added some missing XML documentation

--- src/Serilog.Sinks.LINQPad/Sinks/LINQPad/ColorPair.cs
+++ src/Serilog.Sinks.LINQPad/Sinks/LINQPad/ColorPair.cs
@@ -4,12 +4,27 @@ using System.Drawing;
 namespace Serilog.Sinks.LINQPad
 {
 
+	/// <summary>
+	/// Use to specify a Foreground-Background color pair
+	/// </summary>
 	public class ColorPair
 	{
+		/// <summary>
+		/// Set the foreground color; setting it to null should indicate a "default" or "transparent" color
+		/// </summary>
 		public Color? Foreground { get; set; }
+
+		/// <summary>
+		/// Set the background color; setting it to null should indicate a "default" or "transparent" color
+		/// </summary>
 		public Color? Background { get; set; }
 
 
+		/// <summary>
+		/// Initialize a foreground-background color pair
+		/// </summary>
+		/// <param name="foreground">Set the foreground color; setting it to null should indicate a "default" or "transparent" color</param>
+		/// <param name="background">Set the background color; setting it to null should indicate a "default" or "transparent" color</param>
 		public ColorPair(Color? foreground = null, Color? background = null)
 		{
 			Foreground = foreground;
