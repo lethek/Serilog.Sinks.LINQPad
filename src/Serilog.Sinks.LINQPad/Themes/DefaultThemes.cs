6f96825ec3811d810a313ee25c3d366d5bd3f0f8
Michael Monsour <michael@lethek.net>
2021-11-12 15:23:55 +1000

    Added a new theme in DefaultThemes.LINQPadDark

--- src/Serilog.Sinks.LINQPad/Themes/DefaultThemes.cs
+++ src/Serilog.Sinks.LINQPad/Themes/DefaultThemes.cs
@@ -26,10 +26,31 @@ namespace Serilog.Sinks.LINQPad.Themes
         /// </summary>
         public static ConsoleTheme None { get; } = new EmptyConsoleTheme();
 
-        
+
+
+        public static LINQPadTheme LINQPadDark { get; } = new LINQPadTheme(
+            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle> {
+                [ConsoleThemeStyle.Text] = new LINQPadThemeStyle { Foreground = Color.White },
+                [ConsoleThemeStyle.SecondaryText] = new LINQPadThemeStyle { Foreground = Color.LightGray },
+                [ConsoleThemeStyle.TertiaryText] = new LINQPadThemeStyle { Foreground = Color.Gray },
+                [ConsoleThemeStyle.Invalid] = new LINQPadThemeStyle { Foreground = Color.Red, Background = Color.Yellow, Italic = true },
+                [ConsoleThemeStyle.Null] = new LINQPadThemeStyle { Foreground = Color.Yellow, Italic = true },
+                [ConsoleThemeStyle.Name] = new LINQPadThemeStyle { Foreground = Color.Gray },
+                [ConsoleThemeStyle.String] = new LINQPadThemeStyle { Foreground = Color.Cyan, Bold = true },
+                [ConsoleThemeStyle.Number] = new LINQPadThemeStyle { Foreground = Color.Magenta, Bold = true },
+                [ConsoleThemeStyle.Boolean] = new LINQPadThemeStyle { Foreground = Color.CornflowerBlue, Bold = true },
+                [ConsoleThemeStyle.Scalar] = new LINQPadThemeStyle { Foreground = Color.LightGreen, Bold = true },
+                [ConsoleThemeStyle.LevelVerbose] = new LINQPadThemeStyle { Foreground = Color.Gray },
+                [ConsoleThemeStyle.LevelDebug] = new LINQPadThemeStyle { Foreground = Color.LightGray },
+                [ConsoleThemeStyle.LevelInformation] = new LINQPadThemeStyle { Foreground = Color.DeepSkyBlue },
+                [ConsoleThemeStyle.LevelWarning] = new LINQPadThemeStyle { Foreground = Color.Black, Background = Color.Yellow },
+                [ConsoleThemeStyle.LevelError] = new LINQPadThemeStyle { Foreground = Color.White, Background = Color.Red },
+                [ConsoleThemeStyle.LevelFatal] = new LINQPadThemeStyle { Foreground = Color.White, Background = Color.MediumVioletRed, Bold = true },
+            });
+
+
         public static LINQPadTheme LINQPadLiterate { get; } = new LINQPadTheme(
-            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle>
-            {
+            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle> {
                 [ConsoleThemeStyle.Text] = new LINQPadThemeStyle { Foreground = Color.Black },
                 [ConsoleThemeStyle.SecondaryText] = new LINQPadThemeStyle { Foreground = Color.Gray },
                 [ConsoleThemeStyle.TertiaryText] = new LINQPadThemeStyle { Foreground = Color.DarkGray },
@@ -50,8 +71,7 @@ namespace Serilog.Sinks.LINQPad.Themes
 
 
         public static LINQPadTheme LINQPadColored { get; } = new LINQPadTheme(
-            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle>
-            {
+            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle> {
                 [ConsoleThemeStyle.Text] = new LINQPadThemeStyle { Foreground = Color.Gray },
                 [ConsoleThemeStyle.SecondaryText] = new LINQPadThemeStyle { Foreground = Color.DarkGray },
                 [ConsoleThemeStyle.TertiaryText] = new LINQPadThemeStyle { Foreground = Color.DarkGray },
@@ -72,8 +92,7 @@ namespace Serilog.Sinks.LINQPad.Themes
 
 
         public static LINQPadTheme Literate { get; } = new LINQPadTheme(
-            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle>
-            {
+            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle> {
                 [ConsoleThemeStyle.Text] = new LINQPadThemeStyle { Foreground = Color.White },
                 [ConsoleThemeStyle.SecondaryText] = new LINQPadThemeStyle { Foreground = Color.Gray },
                 [ConsoleThemeStyle.TertiaryText] = new LINQPadThemeStyle { Foreground = Color.DarkGray },
@@ -93,8 +112,7 @@ namespace Serilog.Sinks.LINQPad.Themes
             });
 
         public static LINQPadTheme Grayscale { get; } = new LINQPadTheme(
-            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle>
-            {
+            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle> {
                 [ConsoleThemeStyle.Text] = new LINQPadThemeStyle { Foreground = Color.White },
                 [ConsoleThemeStyle.SecondaryText] = new LINQPadThemeStyle { Foreground = Color.Gray },
                 [ConsoleThemeStyle.TertiaryText] = new LINQPadThemeStyle { Foreground = Color.DarkGray },
@@ -114,8 +132,7 @@ namespace Serilog.Sinks.LINQPad.Themes
             });
 
         public static LINQPadTheme Colored { get; } = new LINQPadTheme(
-            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle>
-            {
+            new Dictionary<ConsoleThemeStyle, LINQPadThemeStyle> {
                 [ConsoleThemeStyle.Text] = new LINQPadThemeStyle { Foreground = Color.Gray },
                 [ConsoleThemeStyle.SecondaryText] = new LINQPadThemeStyle { Foreground = Color.DarkGray },
                 [ConsoleThemeStyle.TertiaryText] = new LINQPadThemeStyle { Foreground = Color.DarkGray },
